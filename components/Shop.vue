9866c999cb95fbffb31cd01ef4921b377513e9a2
liankorExe <lilianblancard@gmail.com>
2025-02-14 00:59:23 +0100

    shop image rotate

--- components/Shop.vue
+++ components/Shop.vue
@@ -8,22 +8,24 @@
 
         <div class="flex flex-col md:flex-row items-center justify-center gap-12">
             <div class="w-full md:w-1/2 flex justify-center">
-                <img src="/shop.png" alt="Shop" class="w-[90%] md:w-[70%] transform -rotate-6 shadow-lg shadow-white/5 rounded-lg">
+                <img src="/shop.png" alt="Shop" class="w-[90%] md:w-[70%] shadow-lg shadow-white/5 rounded-lg"
+                    style="transform: perspective(766px) rotateY(20deg) scale(0.9) rotateX(10deg) translateY(0);">
+
             </div>
 
             <div class="w-full md:w-1/2 space-y-4 bg-white/5 p-6 rounded-lg shadow-md">
                 <p class="text-lg text-gray-200 leading-relaxed">
                     <span class="text-blue-300 font-semibold">Comment ça marche ?</span>
-                    <ul class="list-disc list-inside text-gray-300 mt-2 space-y-1">
-                        <li>Utiliser la touche <UKbd>B</UKbd> pour ouvrir la boutique.</li>
-                        <li>Cliquez sur un sort pour afficher ses détails à droite.</li>
-                        <li>Si vous avez assez de pièces, cliquez sur le <span class="text-green-400 font-semibold">bouton vert</span> pour l'acheter.</li>
-                        <li>Les sorts achetés apparaîtront dans votre inventaire et pourront être utilisés.</li>
-                    </ul>
+                <ul class="list-disc list-inside text-gray-300 mt-2 space-y-1">
+                    <li>Utiliser la touche <UKbd>B</UKbd> pour ouvrir la boutique.</li>
+                    <li>Cliquez sur un sort pour afficher ses détails à droite.</li>
+                    <li>Si vous avez assez de pièces, cliquez sur le <span class="text-green-400 font-semibold">bouton
+                            vert</span> pour l'acheter.</li>
+                    <li>Les sorts achetés apparaîtront dans votre inventaire et pourront être utilisés.</li>
+                </ul>
                 </p>
             </div>
         </div>
         <div class="h-20"></div>
     </div>
 </template>
-
