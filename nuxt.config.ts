2b13d3aea724ea9706a33ade6285ae3cc9af1a89
liankorExe <112548053+liankorExe@users.noreply.github.com>
2025-02-25 11:22:29 +0100

    dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui','@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'dark', // default value of $colorMode.preference
+    fallback: 'dark', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'nuxt-color-mode'
+  }
 })
